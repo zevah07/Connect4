11743d22264f2115e5de46b586baeed31b88a558
Sebastian Braida <sebastianbraida07@gmail.com>
2021-07-03 16:35:54 -0300

    Sigue dando errror

--- Connect4.py
+++ Connect4.py
@@ -33,10 +33,32 @@ def tiroValido(secuencia):
 		if column < 1 & column > 7:
 			return False
 	return True
+    def contenidoColumna(nro_column, tablero):
+    	columns = []
+	for row in tablero:
+		cell = row[nro_column - 1]
+		columns.append(cell)#append sirve para agregar un elemento a una lista
+	return columns
 
-secuencia = [1, 2, 3, 1, 8]
+def contenidoFilas(nro_row, tablero):
+	rows = []
+	for column in tablero:
+		cell = column[nro_row - 1]
+		rows.append(cell)
+	return rows
+
+def ContenidoTodasLasColumnas(tablero):
+	columns = []
+	for nro_column in range(0, 7):
+		columns.insert(7,contenidoColumna(nro_column,tablero))
+	return columns
+
+secuencia = [1, 2, 3, 1]
+tablero = tablerovacio()
 if tiroValido(secuencia):
-	dibujarTablero(completarTableroEnOrden(secuencia,tablerovacio()))
+	tablero = completarTableroEnOrden(secuencia, tablero)
+	dibujarTablero(tablero)
 	print("La secuencia es valida")
 else:                                                        
 	print("Para que la secuencia sea valida los valores tienen que estar comprendidos entre el 1 y el 7") 
+print(contenidoColumna(2, tablero))
